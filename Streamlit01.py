bdf90fbc193e78f95153f8ce94c525abb15eff1e
suthinan07 <156872592+suthinan07@users.noreply.github.com>
2024-01-24 10:25:59 +0700

    Update Streamlit01.py

--- Streamlit01.py
+++ Streamlit01.py
@@ -6,8 +6,14 @@ st.header("การนำเสนอข้อมูลกราฟด้วย
 col1, col2, col3, = st.columns(3)
 
 with col1:
+    st.header("Versicolor")
+    st.image("https://en.wikipedia.org/wiki/Iris_versicolor#/media/File:Blue_Flag,_Ottawa.jpg")
 
 with col2:
+    st.header("Verginica")
+    st.image("https://en.wikipedia.org/wiki/Iris_virginica#/media/File:Iris_virginica_2.jpg") 
 
 with col3:
+    st.header("Setosa")
+    st.image("https://en.wikipedia.org/wiki/Iris_setosa#/media/File:Irissetosa1.jpg")
 
