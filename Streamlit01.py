81c94639dd60ebea0467ba28ffb18b29e59c581b
suthinan07 <156872592+suthinan07@users.noreply.github.com>
2024-01-24 10:09:17 +0700

    Update Streamlit01.py

--- Streamlit01.py
+++ Streamlit01.py
@@ -3,3 +3,11 @@ import streamlit as st
 st.title("การทดสอบสร้างเว็บด้วยPython")
 st.image("data.jpeg")
 st.header("การนำเสนอข้อมูลกราฟด้วย Python") 
+col1, col2, col3, = st.columns(3)
+
+with col1:
+
+with col2:
+
+with col3:
+
