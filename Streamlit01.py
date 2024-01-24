766f826e513f5e39b7b493d13414c1af1aa503cd
suthinan07 <156872592+suthinan07@users.noreply.github.com>
2024-01-24 11:07:32 +0700

    Update Streamlit01.py

--- Streamlit01.py
+++ Streamlit01.py
@@ -1,24 +1,24 @@
 import streamlit as st
+import pandas as pd
 
 st.title("การทดสอบสร้างเว็บด้วยPython")
 st.image("data.jpeg")
 st.header("การนำเสนอข้อมูลกราฟด้วย Python") 
 
-col1, col2, col3, = st.columns(3)
+
+col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.header("Versicolor")
-    st.image("./image2/Versicolor.jpg")
+   st.header("Versicolor")
+   st.image("https://c.pxhere.com/photos/f2/77/iris_flower_purple-1401467.jpg!d")
 
 with col2:
-    st.header("Verginica")
-    st.image("./image2/virginica.jpg") 
+   st.header("Verginica")
+   st.image("https://www.fs.usda.gov/wildflowers/beauty/iris/Blue_Flag/images/iris_virginica/iris_virginica_virginica_lg.jpg")
 
 with col3:
-    st.header("Setosa")
-    st.image("./image2/setosa.jpg")
+   st.header("Setosa")
+   st.image("https://upload.wikimedia.org/wikipedia/commons/4/41/Iris_versicolor_3.jpg")
 
-#import pandas as p
+#import pandas as pd
 df=pd.read_csv("./data/iris.csv")
-st.write(df.head(10))
-
